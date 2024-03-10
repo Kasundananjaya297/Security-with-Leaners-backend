a9b4cbf54416aef317e0b98029e232e984481f29
KasundananjayaT <125995451+Kasundananjaya297@users.noreply.github.com>
2024-03-10 09:45:37 +0530

    Initial commit

---
+++ src/main/java/com/example/SecuritywithLeaners/Controller/AdminServicesController.java
@@ -0,0 +1,48 @@
+package com.example.SecuritywithLeaners.Controller;
+
+
+import com.example.SecuritywithLeaners.DTO.ResponseDTO;
+import com.example.SecuritywithLeaners.DTO.StudentDTO;
+import com.example.SecuritywithLeaners.Entity.Student;
+import com.example.SecuritywithLeaners.Service.AdminService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping( "api/admin")
+@CrossOrigin
+public class AdminServicesController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping("/registerStudent")
+    public ResponseEntity registerStudent(@RequestBody Student studentDTO){
+
+        System.out.println("request received");
+        ResponseDTO responseDTO = adminService.saveStudent(studentDTO);
+        return new ResponseEntity(responseDTO,responseDTO.getStatus());
+    }
+    @GetMapping("/getStudent")
+    public ResponseEntity getAllStudentSize(){
+        ResponseDTO responseDTO = adminService.getAllStudentSize();
+        return new ResponseEntity(responseDTO,responseDTO.getStatus());
+    }
+    @GetMapping("/getStudent/{field}/{order}")
+    public ResponseEntity getStudentWithOrder(@PathVariable String field,@PathVariable String order){
+        ResponseDTO responseDTO = adminService.getStudentBySorting(field,order);
+        return new ResponseEntity(responseDTO,responseDTO.getStatus());
+    }
+
+    @GetMapping("/getStudent/{field}/{order}/{pageSize}/{offset}")
+    public ResponseEntity getStudentWithOrderAndPagination(@PathVariable String field,@PathVariable String order,@PathVariable int pageSize,@PathVariable int offset){
+        ResponseDTO responseDTO = adminService.getStudentBySortingAndPagination(field,order,pageSize,offset);
+        return new ResponseEntity(responseDTO,responseDTO.getStatus());
+    }
+
+
+
+}
---
+++ src/main/java/com/example/SecuritywithLeaners/Repo/StudentRepo.java
@@ -0,0 +1,18 @@
+package com.example.SecuritywithLeaners.Repo;
+
+
+import com.example.SecuritywithLeaners.Entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface StudentRepo extends JpaRepository<Student,String> {
+    @Query(value = "SELECT max(stdID) FROM Student_Details " ,nativeQuery = true)
+    String getMStdID();
+
+    @Query(value = "SELECT *  FROM student_details WHERE Email = LOWER(:email) OR Telephone = (:telephone) OR NIC = (:nic)", nativeQuery = true)
+    Student Available(@Param("email") String email, @Param("telephone") int telephone, @Param("nic") String nic);
+
+}
